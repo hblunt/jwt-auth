1a9480aacd72a472ed137a1c05059236af886deb
hblunt <63094374+hblunt@users.noreply.github.com>
2025-02-05 11:38:24 +1000

    completed

---
+++ authServer.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
+let refreshTokens = [];
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    if (!refreshToken) {
+        return res.sendStatus(401);
+    }
+    if (!refreshTokens.includes(refreshToken)) {
+        return res.sendStatus(403);
+    }
+    jwt.verify(refreshToken, process.env.ACCESS_REFRESH_TOKEN, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        const accessToken = generateAccessToken({ name: user.name });
+        res.json({ accessToken: accessToken });
+    });
+})
+
+app.post('/login', (req, res) => {
+    // Authenticate User
+
+    const username = req.body.username;
+    if (!username) {
+        return res.status(400).json({ error: 'Username is required' });
+    }
+
+    const user = { name: username };
+
+    try {
+        const accessToken = generateAccessToken(user);
+        const refreshToken = jwt.sign(user, process.env.ACCESS_REFRESH_TOKEN);
+        refreshTokens.push(refreshToken);
+        res.json({ 
+            accessToken: accessToken,
+            refreshToken: refreshToken
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to generate access token' });
+    }
+});
+
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '15s' });
+}
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
--- server.js
+++ server.js
@@ -1,6 +1,37 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 
+app.use(express.json()); // Middleware to parse JSON bodies
 
+const posts = [
+    {
+        username: 'cob',
+        content: 'This is the first post'
+    },
+    {
+        username: 'phil',
+        content: 'This is the second post'
+    }
+];
 
-app.listen(5000)
+app.get('/posts', authToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name));
+});
+
+function authToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
